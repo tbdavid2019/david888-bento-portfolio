254fef4b5960b7c7472b837b8f8b2f80bce55cf9
tbdavid2019 <tbdavid2019@gmail.com>
2026-07-01 09:09:50 +0800

    Clear stale service worker cache

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.registration.unregister(),
+      caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))),
+    ]).then(() => self.clients.matchAll({ type: 'window' }))
+      .then((clients) => clients.forEach((client) => client.navigate(client.url))))
+  );
+});
